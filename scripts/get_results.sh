6ac1e982ce0efbc84a1fcae3cab5b8b4908e49f5
Akash Panda <akashpanda@iisc.ac.in>
2021-07-15 20:37:11 +0530

    changes

---
+++ scripts/get_results.sh
@@ -0,0 +1,14 @@
+FOLDER="../evaluation_script/collections_experimnent_data/RESULTS/"
+
+# Figure 3
+bash figure_three.sh > csv/figure_three.csv
+# Figure 4
+bash figure_four.sh > csv/figure_four.csv
+# Figure 6 
+bash figure_six.sh > csv/figure_six.csv
+# Figure 7
+bash figure_seven.sh > csv/figure_seven.csv
+# Figure 8
+bash figure_eight.sh > csv/figure_eight.csv
+# Figure 9
+bash figure_nine.sh > csv/figure_nine.csv
